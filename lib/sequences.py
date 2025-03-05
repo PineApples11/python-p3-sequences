955d83f3793eda3cd5c9833ed2885731cb599cee
PineApples11 <kinyanjuiivy1@gmail.com>
2025-03-05 12:51:16 +0300

    correction

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,11 +1,15 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    
     if length == 0:
         print([])
+        return
     elif length == 1:
-      print([0])
-    elif length == 2:
-        print('0\n1')
+        print([0])
+        return
+    
+    # Start the Fibonacci sequence
+    fib = [0, 1]
+    
+    for _ in range(2, length):
+        fib.append(fib[-1] + fib[-2])  # Sum of last two numbers
 
+    print(fib)  # Ensure the output is in the correct format
